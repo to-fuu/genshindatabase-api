363c9285eeb58371b577a68489e54cd7c2696f88
Ala <chebbi.ala@outlook.com>
2021-01-15 20:31:25 +0100

    added artifact and weapon routing

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 
 const charData = require("./characters.json");
+const artifactData = require("./artifacts.json");
+const materialData = require("./materials.json");
+const weaponData = require("./weapons.json");
 
 let port = process.env.PORT || 3000;
 
@@ -9,8 +12,10 @@ app.get("/", (req, res) => {
   res.send("HelloWorld");
 });
 
+//CHARACTERS
+
 app.get("/characters", (req, res) => {
-  res.send(charData);
+  res.send(charData.characters);
 });
 
 app.get("/characters/:name", (req, res) => {
@@ -31,6 +36,80 @@ app.get("/characters/elements/:element", (req, res) => {
       return item.elementType.toLowerCase() === element.toLowerCase();
     })
   );
+});
+
+app.get("/characters/rarity/:rarity", (req, res) => {
+  var rarity = req.params.rarity;
+
+  res.send(
+    charData.characters.filter(function (item) {
+      return item.rarity == rarity;
+    })
+  );
+});
+
+//ARTIFACTS
+app.get("/artifacts", (req, res) => {
+  res.send(artifactData.artifacts);
+});
+
+app.get("/artifacts/:name", (req, res) => {
+  var name = req.params.name;
+
+  res.send(
+    artifactData.artifacts.filter(function (item) {
+      return item.key.toLowerCase() === name.toLowerCase();
+    })
+  );
+});
+
+app.get("/artifacts/elements/:element", (req, res) => {
+  var element = req.params.element;
+
+  res.send(
+    artifactData.artifacts.filter(function (item) {
+      return item.element.toLowerCase() === element.toLowerCase();
+    })
+  );
+});
+
+app.get("/artifacts/rarity/:rarity", (req, res) => {
+  var rarity = req.params.rarity;
+
+  if (rarity != 4 && rarity != 5) {
+    res.status(404).send("Not found");
+    return;
+  }
+  res.send(
+    artifactData.artifacts.filter(function (item) {
+      return item.rarityMax == rarity;
+    })
+  );
+});
+
+//ARTIFACTS
+app.get("/weapons", (req, res) => {
+  res.send("add a waepon type");
+});
+
+app.get(["/weapons/bow", "/weapons/bows"], (req, res) => {
+  res.send(weaponData.bows);
+});
+
+app.get(["/weapons/swords", "/weapons/sword"], (req, res) => {
+  res.send(weaponData.swords);
+});
+
+app.get(["/weapons/claymores", "/weapons/claymore"], (req, res) => {
+  res.send(weaponData.claymores);
+});
+
+app.get(["/weapons/catalysts", "/weapons/catalyst"], (req, res) => {
+  res.send(weaponData.catalysts);
+});
+
+app.get(["/weapons/polearms", "/weapons/polearm"], (req, res) => {
+  res.send(weaponData.polearms);
 });
 
 app.listen(port, () => {
