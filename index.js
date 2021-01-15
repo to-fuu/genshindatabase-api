a0b2ddaf4edb261d1ff7f3a67e322e7c95a11ae8
Ala <chebbi.ala@outlook.com>
2021-01-15 19:51:05 +0100

    added character routing

--- index.js
+++ index.js
@@ -13,6 +13,26 @@ app.get("/characters", (req, res) => {
   res.send(charData);
 });
 
+app.get("/characters/:name", (req, res) => {
+  var name = req.params.name;
+
+  res.send(
+    charData.characters.filter(function (item) {
+      return item.name.toLowerCase() === name.toLowerCase();
+    })
+  );
+});
+
+app.get("/characters/elements/:element", (req, res) => {
+  var element = req.params.element;
+
+  res.send(
+    charData.characters.filter(function (item) {
+      return item.elementType.toLowerCase() === element.toLowerCase();
+    })
+  );
+});
+
 app.listen(port, () => {
   console.log(`example app is listening on port ${port}`);
 });
