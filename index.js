02670f77709b2619e3e0e1d1104314742faced2b
Ala <chebbi.ala@outlook.com>
2021-01-15 19:00:22 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+const charData = require("./characters.json");
+
+let port = process.env.port || 3000;
+
+app.get("/", (req, res) => {
+  res.send("HelloWorld");
+});
+
+app.get("/characters", (req, res) => {
+  res.send(charData);
+});
+
+app.listen(port, () => {
+  console.log(`example app is listening on port ${port}`);
+});
